1d36f4b9ca48f8c4d6d658d6d7e3e218b0d8fa8c
Mouss <104678778+MoussCarter@users.noreply.github.com>
2023-06-05 09:55:22 +0200

    history actions

--- src/pages/Game/Game.jsx
+++ src/pages/Game/Game.jsx
@@ -5,19 +5,39 @@ import "./Game.scss";
 const Game = () => {
   const [isX, setIsX] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
 
   const handlePlay = (updatedSquares) => {
-    setHistory([...history, updatedSquares]);
+    const nextHistory = [...history.slice(0, currentMove + 1), updatedSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
     setIsX(!isX);
   };
+
+  const jumpTo = (nextMove) => {
+    setCurrentMove(nextMove);
+    setIsX(nextMove % 2 === 0);
+  };
+
+  const moves = history.map((squares, move) => {
+    let description;
+    description = move > 0 ? `Go back to ${move}` : `Go to game start`;
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description} </button>
+      </li>
+    );
+  });
 
   return (
     <div className="game">
       <div className="game__board">
         <Board isX={isX} squares={currentSquares} onPlay={handlePlay} />
       </div>
-      <div className="game__info"></div>
+      <div className="game__info">
+        <ol>{moves}</ol>
+      </div>
     </div>
   );
 };
